dbe734e7a40041ee9f7087329f5956f9588b8483
Michael Mulatya <michaelmulatz@gmail.com>
2019-08-18 09:35:28 +0300

    initial project version

--- pbgreservation/urls.py
+++ pbgreservation/urls.py
@@ -18,7 +18,8 @@ from django.urls import include,path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-	 path('reservations/',include('reservations.urls')),
+    path('',include('reservations.urls')),
+    path('reservations/',include('reservations.urls')),
     path('settings/', admin.site.urls),
     path('login/',auth_views.LoginView.as_view(),name='login',kwargs={"redirect_authenticated_user":True}),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
