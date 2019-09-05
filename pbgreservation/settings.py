9ece5f7dd6b80469cf0d15809aa6a41e41990362
Michael Mulatya <michaelmulatz@gmail.com>
2019-09-05 13:26:40 +0300

    initial version

--- pbgreservation/settings.py
+++ pbgreservation/settings.py
@@ -137,3 +137,11 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'michaelmulatz@gmail.com'
+EMAIL_HOST_PASSWORD = 'muratina13
