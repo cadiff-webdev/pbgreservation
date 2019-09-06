a3515ef5f8c94527d6087b6ecc288dbc968249e6
Michael Mulatya <michaelmulatz@gmail.com>
2019-09-06 10:28:02 +0300

    initial version

--- pbgreservation/settings.py
+++ pbgreservation/settings.py
@@ -140,8 +140,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'michaelmulatz@gmail.com'
+EMAIL_HOST = 'mail.kilelehomestay.com'
+EMAIL_USE_SSL = True
+EMAIL_PORT = 456
+EMAIL_HOST_USER = 'pbg@pbgtest.kilelehomestay.com'
 EMAIL_HOST_PASSWORD = 'muratina13'
